783303976287af5198f570e956105f1609b711a1
ki_tom_com_pc <2240455331@qq.com>
2014-12-01 09:20:07 +0800

    version 1.0

---
+++ amapyuntuitaste/src/com/itaste/yuntu/LoadActivity.java
@@ -0,0 +1,29 @@
+package com.itaste.yuntu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class LoadActivity extends Activity {
+	private static final int LOAD_DISPLAY_TIME = 1500;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 getWindow().setFormat(PixelFormat.RGBA_8888);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+		setContentView(R.layout.activity_load);
+		
+	    new Handler().postDelayed(new Runnable() {
+	    	             public void run() {
+	    	                 /* Create an Intent that will start the Main WordPress Activity. */
+	    	                 Intent mainIntent = new Intent(LoadActivity.this, MainActivity.class);
+	    	                 LoadActivity.this.startActivity(mainIntent);
+	    	                 LoadActivity.this.finish();
+	    	             }
+	    	         }, LOAD_DISPLAY_TIME); //1500 for release
+	    	 
+	}
+}
