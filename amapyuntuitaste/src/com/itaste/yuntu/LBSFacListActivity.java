cf09fee77de42b182be3df47c607bee858b4fb6f
ki_tom_com_pc <2240455331@qq.com>
2014-12-04 10:54:40 +0800

    last commit

--- amapyuntuitaste/src/com/itaste/yuntu/LBSFacListActivity.java
+++ amapyuntuitaste/src/com/itaste/yuntu/LBSFacListActivity.java
@@ -1,69 +1,85 @@
 package com.itaste.yuntu;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.itaste.yuntu.adapter.FacBaseListAdapter;
 import com.itaste.yuntu.util.ItasteApplication;
 import com.itaste.yuntu.util.LBSCloudUtils;
 
-public class LBSFacListActivity extends Activity {
+public class LBSFacListActivity extends Activity 
+implements OnRefreshListener,OnScrollListener  {
 	private ListView faclv;
+	private SwipeRefreshLayout swipe_ly;
+	private ProgressBar load_data_pg;
+	private BaseAdapter listAdapter;
+	// 最后可见条目的索引
+    private int lastVisibleIndex;
+	// ListView底部View
+    private View moreView;
 	public final static int CALL_PHONE=1;
+	private static final int REFRESH_COMPLETE = 1;
+	private Handler mHandler = new Handler(){
+		public void handleMessage(android.os.Message msg){
+			switch (msg.what)
+			{
+			case REFRESH_COMPLETE:
+				ItasteApplication.getInstance().resetLoad();
+				LBSCloudUtils.search(LBSFacListActivity.this,LBSCloudUtils.currSearchType);
+				listAdapter.notifyDataSetChanged();
+				swipe_ly.setRefreshing(false);
+				break;
+
+			}
+		}
+	};
+	
+	
+	
+	@SuppressLint("InlinedApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fac_list);
-		faclv = (ListView) this.findViewById(R.id.faclv);
 		ItasteApplication application = ItasteApplication.getInstance();
-		BaseAdapter listAdapter = new FacBaseListAdapter(this,application.getValidateDto().getDatas());
+		faclv = (ListView) this.findViewById(R.id.faclv);
+		
+		//上方刷新控件
+		swipe_ly = (SwipeRefreshLayout) this.findViewById(R.id.swipe_ly);
+		swipe_ly.setOnRefreshListener(this);
+		swipe_ly.setColorScheme(android.R.color.holo_blue_bright, android.R.color.holo_green_light,
+				android.R.color.holo_orange_light, android.R.color.holo_red_light);
+		//下方加载更多数据view
+		moreView = getLayoutInflater().inflate(R.layout.more_data, null);
+		load_data_pg = (ProgressBar) moreView.findViewById(R.id.load_data_pg);
+		listAdapter = new FacBaseListAdapter(this,application.getValidateDto().getDatas());
+		faclv.addFooterView(moreView);
 		faclv.setAdapter(listAdapter);
 		application.listAdapter = listAdapter;
+		faclv.setOnScrollListener(this);
+		
 	}
 	@Override
-	protected void onResume() {
+	protected void onResume(){
 		super.onResume();
 		final ItasteApplication application = ItasteApplication.getInstance();
 		BaseAdapter listAdapter = application.listAdapter;
 		if (listAdapter!=null) {
 		listAdapter.notifyDataSetChanged();
 		}
-		if(application.getValidateDto().getCount()<=0&&!application.istosearch){
-			AlertDialog.Builder builder = new Builder(this);
-			builder.setIcon(R.drawable.ic_launcher)
-				   .setTitle("提示")
-				   .setMessage("尚无数据，是否立即搜索！")
-				   .setPositiveButton("确定", new OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface arg0, int arg1) {
-						application.istosearch = true;
-						startActivityForResult(new Intent(LBSFacListActivity.this, SearchActivity.class), ItasteApplication.List_REQUEST_SEARCH_CODE);
-						
-					}
-				   }).setNegativeButton("稍后", new OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface arg0, int arg1) {
-						
-					}
-				   });
-			builder.create().show();
-	
-		}else{
-			application.istosearch = false;
+		if(application.getValidateDto().getCount()<=0){
+			LBSCloudUtils.search(this,LBSCloudUtils.SEARCH_TYPE_ALL_LIST);
 		}
 	}
 	@Override
@@ -71,5 +87,40 @@ public class LBSFacListActivity extends Activity {
 		if(ItasteApplication.List_REQUEST_SEARCH_CODE==requestCode&&resultCode==ItasteApplication.SEARCH_FAC_RESULT_CODE){
 			LBSCloudUtils.search(this,LBSCloudUtils.SEARCH_TYPE_ALL_LIST);
 		}
+	}
+	//下拉刷新
+	@Override
+	public void onRefresh() {
+		Toast.makeText(this, "加载最新......", Toast.LENGTH_LONG).show();
+		mHandler.sendEmptyMessageDelayed(REFRESH_COMPLETE, 2000);
+	}
+	
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		// 滑到底部后自动加载，判断listview已经停止滚动并且最后可视的条目等于adapter的条目
+        int count = listAdapter.getCount();
+		int totalCount = ItasteApplication.getInstance().getTotalCount();
+		if (scrollState == OnScrollListener.SCROLL_STATE_IDLE
+                && lastVisibleIndex == count
+                &&totalCount>count
+                ) {
+        	ItasteApplication.getInstance().setNextStepPage(1);
+        	load_data_pg.setVisibility(View.VISIBLE);
+        	LBSCloudUtils.search(this,LBSCloudUtils.currSearchType);
+        	Toast.makeText(this, "狂力加载中......！", Toast.LENGTH_LONG).show();
+        }
+		  // 所有的条目已经和最大条数相等，则移除底部的View
+        if (scrollState == OnScrollListener.SCROLL_STATE_IDLE&&
+        		lastVisibleIndex>0&&totalCount==lastVisibleIndex) {
+        	moreView.setVisibility(View.GONE);
+            Toast.makeText(this, "数据全部加载完成，没有更多数据！", Toast.LENGTH_LONG).show();
+        }
+		
+	}
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
+		 // 计算最后可见条目的索引
+        lastVisibleIndex = firstVisibleItem + visibleItemCount - 1;
 	}
 }
